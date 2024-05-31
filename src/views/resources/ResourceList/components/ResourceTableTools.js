b14874a4878110f398dfebe1e57084365d526723
earthcomfy <hanagetbel93@gmail.com>
2024-05-31 13:09:56 +0300

    Comment out resource filter

--- src/views/resources/ResourceList/components/ResourceTableTools.js
+++ src/views/resources/ResourceList/components/ResourceTableTools.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Button } from "components/ui";
 import { HiPlusCircle } from "react-icons/hi";
 import ResourceTableSearch from "./ResourceTableSearch";
-import ResourceFilter from "./ResourceFilter";
+// import ResourceFilter from "./ResourceFilter";
 import { Link } from "react-router-dom";
 
 const ResourceTableTools = () => {
   return (
     <div className="flex flex-col md:flex-row gap-2 md:items-center">
       <ResourceTableSearch />
-      <ResourceFilter />
+      {/* <ResourceFilter /> */}
       <Link className="block lg:inline-block md:mb-0 mb-4" to="/resource-new">
         <Button block variant="solid" size="sm" icon={<HiPlusCircle />}>
           Add Resource
